3d6f9567eba4213516cab594482dbca3c6a2e24e
pocha <pocha.sharma@gmail.com>
2017-02-26 03:05:18 +0530

    gofmtised the source code

--- main.go
+++ main.go
@@ -1,132 +1,130 @@
 package main
 
 import (
+	"bytes"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
 	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
-  "crypto/hmac"
-  "crypto/sha256"
-  "encoding/base64"
-  "time"
-  "strings"
-  "io/ioutil"
-  "bytes"
+	"strings"
+	"time"
 )
 
 const auth_username = "ashish"
 const auth_password = "34b5db01-8bc3-4960-92d7-d83155a42dfe"
 
 type (
-  SMS struct {
-    From string `json:"from"`
-    To   string `json:"to"`
-    Message string `json:"message"`
-  }
+	SMS struct {
+		From    string `json:"from"`
+		To      string `json:"to"`
+		Message string `json:"message"`
+	}
 )
 
-func buildHash(data []byte) string{
-  mac := hmac.New(sha256.New, []byte(auth_password))
-  mac.Write(data)
-  return base64.URLEncoding.EncodeToString(mac.Sum(nil))
+func buildHash(data []byte) string {
+	mac := hmac.New(sha256.New, []byte(auth_password))
+	mac.Write(data)
+	return base64.URLEncoding.EncodeToString(mac.Sum(nil))
 }
 
 func exitWithJson(w http.ResponseWriter, message string, error string) {
-  _output := map[string]string { "message": message, "error": error}
-  output, _ := json.Marshal(_output)
-  w.Header().Set("Content-Type", "application/json")
-  w.Write(output)
+	_output := map[string]string{"message": message, "error": error}
+	output, _ := json.Marshal(_output)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(output)
 }
 
-func sendToKannel(data SMS) bool{
-  //simulating 1 sec delay
-  time.Sleep(1 * time.Second)
-  return true
+func sendToKannel(data SMS) bool {
+	//simulating 1 sec delay
+	time.Sleep(1 * time.Second)
+	return true
 }
 
 func getBodyJson(r *http.Request) []byte {
-  bodyBytes, _ := ioutil.ReadAll(r.Body)
-  r.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
-  return bodyBytes
+	bodyBytes, _ := ioutil.ReadAll(r.Body)
+	r.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
+	return bodyBytes
 }
 
 func SMSHandler(w http.ResponseWriter, r *http.Request) {
 
-  if r.Header.Get("Content-Type") != "application/json" {
-    http.Error(w, "only json data accepted", 400)
-    return
-  }
-  
-  var data SMS
-  _ = json.Unmarshal(getBodyJson(r), &data)
-  log.Println("sms data", data)
+	if r.Header.Get("Content-Type") != "application/json" {
+		http.Error(w, "only json data accepted", 400)
+		return
+	}
 
-  var sms_map map[string]interface{}
-  err := json.Unmarshal(getBodyJson(r),&sms_map)
-  if err != nil {
-    http.Error(w, "invalid json", 400)
-    return
-  }
-  log.Println("sms_map", sms_map)
+	var data SMS
+	_ = json.Unmarshal(getBodyJson(r), &data)
+	log.Println("sms data", data)
 
-    
-  hash := r.FormValue("hash")
-  if hash == "" {
-    http.Error(w, "hash value as parameter expected", 400)
-    return
-  }
-  
-  b, _ := json.Marshal(data)
-  expectedHash := buildHash(b)
-  log.Println("hash value", hash)
-  log.Println("expt value", expectedHash)
+	var sms_map map[string]interface{}
+	err := json.Unmarshal(getBodyJson(r), &sms_map)
+	if err != nil {
+		http.Error(w, "invalid json", 400)
+		return
+	}
+	log.Println("sms_map", sms_map)
 
-  //check hash
-  if strings.Compare(hash, expectedHash) != 0{
-    http.Error(w, "authentication failed", 400)
-    return
-  }
-  
+	hash := r.FormValue("hash")
+	if hash == "" {
+		http.Error(w, "hash value as parameter expected", 400)
+		return
+	}
 
-  //check for elements presence
-  _, ok := sms_map["from"]
-  if ok == false {
-    exitWithJson(w,"","from field absent")
-    return
-  }
-  _, ok = sms_map["to"]
-  if ok == false {
-    exitWithJson(w,"","to field absent")
-    return
-  }
-  _, ok = sms_map["message"]
-  if ok == false {
-    exitWithJson(w,"","message field absent")
-    return
-  }
+	b, _ := json.Marshal(data)
+	expectedHash := buildHash(b)
+	log.Println("hash value", hash)
+	log.Println("expt value", expectedHash)
 
-  //validate elements - not sure why the count yields one more digit
-  if strings.Count(data.From,"") != 13 {
-    exitWithJson(w,"","invalid from field")
-    return
-  }
-  if strings.Count(data.To,"") != 13 {
-    exitWithJson(w,"","invalid to field")
-    return
-  }
-  //no idea how to check if message text is utf16 or gsm7 supported
-  
-  //everything looks fine - send data to kannel
-  if sendToKannel(data)  {
-    exitWithJson(w,"outbound sms ok","")
-  } else {
-    exitWithJson(w,"","unknown error occured")
-  }
- 
+	//check hash
+	if strings.Compare(hash, expectedHash) != 0 {
+		http.Error(w, "authentication failed", 400)
+		return
+	}
+
+	//check for elements presence
+	_, ok := sms_map["from"]
+	if ok == false {
+		exitWithJson(w, "", "from field absent")
+		return
+	}
+	_, ok = sms_map["to"]
+	if ok == false {
+		exitWithJson(w, "", "to field absent")
+		return
+	}
+	_, ok = sms_map["message"]
+	if ok == false {
+		exitWithJson(w, "", "message field absent")
+		return
+	}
+
+	//validate elements - not sure why the count yields one more digit
+	if strings.Count(data.From, "") != 13 {
+		exitWithJson(w, "", "invalid from field")
+		return
+	}
+	if strings.Count(data.To, "") != 13 {
+		exitWithJson(w, "", "invalid to field")
+		return
+	}
+	//no idea how to check if message text is utf16 or gsm7 supported
+
+	//everything looks fine - send data to kannel
+	if sendToKannel(data) {
+		exitWithJson(w, "outbound sms ok", "")
+	} else {
+		exitWithJson(w, "", "unknown error occured")
+	}
+
 }
 
 func main() {
-  http.HandleFunc("/outbound/sms", SMSHandler)
+	http.HandleFunc("/outbound/sms", SMSHandler)
 
-  log.Println("starting server")
-  log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Println("starting server")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- main_test.go
+++ main_test.go
@@ -1,64 +1,61 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	"github.com/stretchr/testify/assert"
-  "encoding/json"
-  "bytes"
-  "fmt"
 )
-
-
 
 func TestSMSHandler(t *testing.T) {
 	assert := assert.New(t)
 
-  validData := SMS {
-                  From: "919538384545",
-                  To:   "919845350048",
-                  Message:  "hello how are you",
-                }
+	validData := SMS{
+		From:    "919538384545",
+		To:      "919845350048",
+		Message: "hello how are you",
+	}
 
-
-  jsonData, _ := json.Marshal(validData)
-  validHash := buildHash(jsonData)
-  fmt.Println("validHash", validHash)
+	jsonData, _ := json.Marshal(validData)
+	validHash := buildHash(jsonData)
+	fmt.Println("validHash", validHash)
 
 	successTests := []struct {
 		description        string
 		url                string
-    input              SMS
+		input              SMS
 		expectedStatusCode int
-    output             map[string]string
+		output             map[string]string
 	}{
 		{
 			description:        "valid test data",
 			url:                "/outbound/sms?hash=" + validHash,
-      input:              validData,
+			input:              validData,
 			expectedStatusCode: 200,
-      output:             map[string]string{ "message" : "outbound sms ok", "error" : "" },
-		}, 	
-  }
+			output:             map[string]string{"message": "outbound sms ok", "error": ""},
+		},
+	}
 
 	for _, tc := range successTests {
-    
-    input, err := json.Marshal(tc.input)
-    if err != nil {
-      fmt.Println("error encounted", err)
-    }
-     
+
+		input, err := json.Marshal(tc.input)
+		if err != nil {
+			fmt.Println("error encounted", err)
+		}
+
 		req, err := http.NewRequest("POST", tc.url, bytes.NewBuffer(input))
 		assert.NoError(err)
-    req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Content-Type", "application/json")
 
 		w := httptest.NewRecorder()
 		SMSHandler(w, req)
 
 		assert.Equal(tc.expectedStatusCode, w.Code, tc.description)
-    
-    output, _ := json.Marshal(tc.output)
+
+		output, _ := json.Marshal(tc.output)
 		assert.Equal(string(output), w.Body.String(), tc.description)
 	}
 }
